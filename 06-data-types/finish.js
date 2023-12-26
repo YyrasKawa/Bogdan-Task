1d4554dcd465a7558c7b64bd4219d43a01a5f8ec
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-26 12:09:18 +0100

    Task solution 5,6

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,25 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+
+const myString = 'It is my string';
+const myNumber = 41;
+const myBoolean = true;
+const myNull = null;
+let myUndefined;
+const myObject = {
+    name: 'Yury',
+    age: 41,
+    city: 'Warsaw'
+}
+
+const myArray = ['Yury', 41, 'Warsaw'];
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefined);
+console.log(typeof myObject);
+console.log(typeof myArray);
